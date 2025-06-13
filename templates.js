b5009da65170b3a1c8a341d93f55455b70870753
AmberS11 <ambersinha11@gmail.com>
2025-06-13 15:00:15 +0530

    Update templates.js

--- templates.js
+++ templates.js
@@ -1,1 +1,30 @@
-// templates.js
+import Clean from './clean';
+import Classic from './classic';
+import Creative from './creative';
+import Functional from './functional';
+import Modern from './modern';
+
+const templates = {
+  clean: {
+    name: 'Clean',
+    component: Clean,
+  },
+  classic: {
+    name: 'Classic',
+    component: Classic,
+  },
+  creative: {
+    name: 'Creative',
+    component: Creative,
+  },
+  functional: {
+    name: 'Functional',
+    component: Functional,
+  },
+  modern: {
+    name: 'Modern',
+    component: Modern,
+  },
+};
+
+export default templates;
