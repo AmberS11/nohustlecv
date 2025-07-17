ad25778add53e97228154b292449d37673cea4cb
AmberS11 <ambersinha11@gmail.com>
2025-07-17 10:59:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
 // next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'res.cloudinary.com'], // Add more domains if hosting images externally
+  },
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
